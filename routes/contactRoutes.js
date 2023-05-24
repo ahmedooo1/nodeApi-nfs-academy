f003b9a432fd5b397df321afc783fc6991724020
ahmedooo1 <94185300+ahmedooo1@users.noreply.github.com>
2023-05-24 15:06:37 +0200

    swagger add to contact route

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,6 +3,33 @@ const contactController = require('../controllers/contactController');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /contact:
+ *   post:
+ *     summary: Send a contact message
+ *     tags: [Contacts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               message:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Contact message sent successfully
+ *       400:
+ *         description: Invalid request body
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/contact', contactController.sendContactMessage);
 
 module.exports = router;
