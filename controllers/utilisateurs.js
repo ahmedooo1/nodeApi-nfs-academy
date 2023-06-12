9db44872ffda0744aa14bdd53a17c847cbbb4609
ahmedooo1 <94185300+ahmedooo1@users.noreply.github.com>
2023-06-12 19:04:25 +0200

    user added to the login 'utilisateur' + port n° changed

--- controllers/utilisateurs.js
+++ controllers/utilisateurs.js
@@ -6,21 +6,21 @@ const jwt = require('jsonwebtoken')
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        const user = new User({
-            name : req.body.name,
-          email: req.body.email,
-          password: hash,
-          
-        });
-        user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
-  };
+        .then(hash => {
+            const user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: hash,
 
-  exports.login = (req, res, next) => {
+            });
+            user.save()
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
@@ -33,57 +33,61 @@ exports.signup = (req, res, next) => {
                     }
                     res.status(200).json({
                         userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            process.env.JWT_SECRET,
-                            { expiresIn: '24h' }
-                        )
+                        token: jwt.sign({ userId: user._id },
+                            process.env.JWT_SECRET, { expiresIn: '24h' }
+                        ),
+                        user: {
+                            _id: user._id,
+                            email: user.email,
+                            name: user.name
+                        }
                     });
                 })
                 .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
- };
+};
 
 
 
- exports.getUsers = (req, res, next) => {
+exports.getUsers = (req, res, next) => {
     User.find()
-      .then(users => res.status(200).json(users))
-      .catch(error => res.status(400).json({ error }));
-  } 
-  exports.getUser = (req, res, next) => {
+        .then(users => res.status(200).json(users))
+        .catch(error => res.status(400).json({ error }));
+}
+exports.getUser = (req, res, next) => {
     User.findById(req.params.id)
-      .then(user => res.status(200).json(user))
-      .catch(error => res.status(404).json({ error }));
-  }
- exports.updateUser = (req, res) => {
-    User.findByIdAndUpdate(req.params.id, req.body, { new: true})
-      .then(user => res.status(200).json(user))
-      .catch(err => res.status(400).json(err));
-  };
+        .then(user => res.status(200).json(user))
+        .catch(error => res.status(404).json({ error }));
+}
+exports.updateUser = (req, res) => {
+    User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(user => res.status(200).json(user))
+        .catch(err => res.status(400).json(err));
+};
 
 
 
-  exports.deleteUser = (req, res, next) => {
+exports.deleteUser = (req, res, next) => {
     User.findByIdAndRemove(req.params.id)
-      .then(() => res.status(200).json({ message: 'Utilisateur supprimé !' }))
-      .catch(error => res.status(400).json({ error }));
-  }
+        .then(() => res.status(200).json({ message: 'Utilisateur supprimé !' }))
+        .catch(error => res.status(400).json({ error }));
+}
 
-  exports.searchUsers = (req, res) => {
+exports.searchUsers = (req, res) => {
     const searchTerm = req.body.searchTerm;
     const role = req.body.role;
-    
-    User.find({ 
-      $or: [
-        { $or: [
-          { name: { $regex: searchTerm, $options: 'i' } },
-          { email: { $regex: searchTerm, $options: 'i' } } 
-        ] },
-        { role: role }
-      ] 
-    })
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(400).json(err)); 
-  };
+
+    User.find({
+            $or: [{
+                    $or: [
+                        { name: { $regex: searchTerm, $options: 'i' } },
+                        { email: { $regex: searchTerm, $options: 'i' } }
+                    ]
+                },
+                { role: role }
+            ]
+        })
+        .then(users => res.status(200).json(users))
+        .catch(err => res.status(400).json(err));
+};
--- models/guides.js
+++ models/guides.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 const thingSchema = mongoose.Schema({
     category: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category'  // Référence au modèle Category
-      },
+        ref: 'Category' // Référence au modèle Category
+    },
 
-    title: {type: String, required: true},
-    description:{type: String, required: true},
-    imageUrl: {type: String},
-    price:{type: Number, required:true},
-    userId:{type: String}
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String },
+    price: { type: Number, required: true },
+    userId: { type: String }
 
 });
 
-module.exports= mongoose.model('Thing', thingSchema);
+module.exports = mongoose.model('Thing', thingSchema);
