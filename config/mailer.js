045cbe84efa5c4986ba39066d51062dd3c808155
ahmedooo1 <aahmadooo997@gmail.com>
2023-07-11 02:02:43 +0200

    Update mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -1,11 +1,17 @@
 const nodemailer = require("nodemailer");
 require('dotenv').config();
 
+
 const transporter = nodemailer.createTransport({
-    service: "Outlook365", // Pour Outlook.com, utilisez "Outlook365"
-    auth: {
-      user: process.env.MAILER_EMAIL, // Remplacez par votre adresse e-mail Outlook
-      pass: process.env.MAILER_SECRET, // Remplacez par le mot de passe de votre e-mail Outlook
-    },
-  });
-module.exports = transporter;
+  service: "Outlook365",
+  secure: true,
+  auth: {
+      user: process.env.MAILER_EMAIL,
+      pass: process.env.MAILER_SECRET,
+  },
+  tls: {
+      rejectUnauthorized: false,
+  },
+});
+
+
